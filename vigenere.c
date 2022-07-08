e0581754250f0a4d1db4cbb1255e3396a2fef862
Huang-Tsu <huang0966714@gmail.com>
2022-07-08 17:29:22 +0800

    change control flow of vigenere

--- vigenere.c
+++ vigenere.c
@@ -13,6 +13,7 @@ void EnOrDecode(char *text, char *key, int mode){
   for(int i=0; i<key_len; i++){
     key_value[i] = (strchr(g_alphabet, key[i])-g_alphabet) * mode; 
   }
+  printf("result:\033[0;32m");  //set result to green
   for(int i=0; i<text_len; i++){
     value = (((strchr(g_alphabet, text[i]) - g_alphabet) + key_value[j]) + g_alphabet_len) % g_alphabet_len; //((text_value)+key_value) % alphabet_len
     if(value<1){
@@ -24,7 +25,7 @@ void EnOrDecode(char *text, char *key, int mode){
     if(j == key_len-1) j = 0;
     else j ++;
   }
-  puts("");
+  printf("\033[0m\n");  //set color back to default
 }
 int main(){
   char text[4096];
@@ -34,22 +35,33 @@ int main(){
   printf("Would you like to encode or decode [e/d]? ");
     scanf("%c", &mode); 
   while(mode != 'e' && mode != 'd'){
-    printf("Please input 'e' for encode or 'd' for decode");
+    getchar(); //skip new_line
+    printf("Please input 'e' for encode or 'd' for decode\n");
+    printf("Would you like to encode or decode [e/d]? ");
     scanf("%c", &mode); 
+  }
+
+  printf("Please input key, consisting of [A-Za-z0-9/:%%.-+=]: ");
+  scanf("%s", key);
+  for(int i=0; i<strlen(key); i++){
+    if(!strchr(g_alphabet, key[i])){
+      printf("key contain '%c', invalid, byebye!\n", key[i]);   
+      exit(1);
+    }
   }
   getchar(); //skip new_line
 
   printf("Please input text(only one line is allowed):");
-  fgets(text, 4096, stdin);
-  if(text[strlen(text)-1] == '\n') text[strlen(text)-1] = '\0'; //remove new line
+  while(fgets(text, 4096, stdin) != NULL){
+    if(text[strlen(text)-1] == '\n') text[strlen(text)-1] = '\0'; //remove new line
 
-  printf("Please input key(alphabet only):");
-  scanf("%s", key);
-  if(mode == 'e'){
-    EnOrDecode(text, key, 1);
-  }
-  else{
-    EnOrDecode(text, key, -1);
+    if(mode == 'e'){
+      EnOrDecode(text, key, 1);
+    }
+    else{
+      EnOrDecode(text, key, -1);
+    }
+    printf("You can input next text: ");
   }
 
   return 0;
